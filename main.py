89222e0587245659147a6307f3b185404dff6b4e
kazu-321 <kzs321kzs@gmail.com>
2024-06-01 14:14:29 +0900

    Refactor assembly class to Assembler class and implement basic instructions

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-class assembly:
+class Assembler:
     def __init__(self):
-        self.REG=[]
-        self.OUTPUT=[]
-    
-    def load(self,reg,value):
-        if reg>len(self.REG)-1:
-            while reg>len(self.REG):
-                self.REG.append("")
-            self.REG.append(value)
+        self.REG = [0] * 10
+        self.MEM = [0] * 100
+        self.PC = 0
+
+    def LOAD(self, reg, value):
+        self.REG[reg] = value
+
+    def STORE(self, reg, addr):
+        self.MEM[addr] = self.REG[reg]
+
+    def ADD(self, reg1, reg2, reg3):
+        self.REG[reg3] = self.REG[reg1] + self.REG[reg2]
+
+    def SUB(self, reg1, reg2, reg3):
+        self.REG[reg3] = self.REG[reg1] - self.REG[reg2]
+
+    def MUL(self, reg1, reg2, reg3):
+        self.REG[reg3] = self.REG[reg1] * self.REG[reg2]
+
+    def DIV(self, reg1, reg2, reg3):
+        self.REG[reg3] = self.REG[reg1] / self.REG[reg2]
+
+    def JMP(self, addr):
+        self.PC = addr
+
+    def JZ(self, reg, addr):
+        if self.REG[reg] == 0:
+            self.PC = addr
+
+    def JNZ(self, reg, addr):
+        if self.REG[reg] != 0:
+            self.PC = addr
+
+    def COMP(self, reg1, reg2):
+        if self.REG[reg1] < self.REG[reg2]:
+            self.FLAG = -1
+        elif self.REG[reg1] > self.REG[reg2]:
+            self.FLAG = 1
         else:
-            self.REG[reg]=value
+            self.FLAG = 0
+
+    def HLT(self):
+        pass
