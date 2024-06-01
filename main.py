a8534d1add4e90cca23c4780189075660647ed9f
kazu-321 <kzs321kzs@gmail.com>
2024-06-01 14:22:49 +0900

    Refactor assembly class to Assembler class and implement basic instructions

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ class Assembler:
         self.REG = [0] * 10
         self.MEM = [0] * 100
         self.PC = 0
+        self.FLAG = 0
 
-    def LOAD(self, reg, value):
-        self.REG[reg] = value
-
-    def STORE(self, reg, addr):
-        self.MEM[addr] = self.REG[reg]
+    def MOV(self,value1,value2):
+        if value1[0]=="[" and value1[-1]=="]":
+            if value2[0]=="[" and value2[-1]=="]":
+                self.MEM[int(value1[1:-1])] = self.MEM[int(value2[1:-1])]
+            else:
+                self.MEM[int(value1[1:-1])] = self.REG[int(value2)]
+        else:
+            if value2[0]=="[" and value2[-1]=="]":
+                self.REG[int(value1)] = self.MEM[int(value2[1:-1])]
+            else:
+                self.REG[int(value1)] = self.REG[int(value2)]
 
     def ADD(self, reg1, reg2, reg3):
         self.REG[reg3] = self.REG[reg1] + self.REG[reg2]
@@ -20,7 +27,7 @@ class Assembler:
         self.REG[reg3] = self.REG[reg1] * self.REG[reg2]
 
     def DIV(self, reg1, reg2, reg3):
-        self.REG[reg3] = self.REG[reg1] / self.REG[reg2]
+        self.REG[reg3] = self.REG[reg1] // self.REG[reg2]
 
     def JMP(self, addr):
         self.PC = addr
@@ -43,3 +50,29 @@ class Assembler:
 
     def HLT(self):
         pass
+
+    def run(self,code):
+        while self.PC < len(code):
+            inst = code[self.PC]
+            if inst[0] == "MOV":
+                self.MOV(inst[1],inst[2])
+            elif inst[0] == "ADD":
+                self.ADD(int(inst[1]),int(inst[2]),int(inst[3]))
+            elif inst[0] == "SUB":
+                self.SUB(int(inst[1]),int(inst[2]),int(inst[3]))
+            elif inst[0] == "MUL":
+                self.MUL(int(inst[1]),int(inst[2]),int(inst[3]))
+            elif inst[0] == "DIV":
+                self.DIV(int(inst[1]),int(inst[2]),int(inst[3]))
+            elif inst[0] == "JMP":
+                self.JMP(int(inst[1]))
+            elif inst[0] == "JZ":
+                self.JZ(int(inst[1]),int(inst[2]))
+            elif inst[0] == "JNZ":
+                self.JNZ(int(inst[1]),int(inst[2]))
+            elif inst[0] == "COMP":
+                self.COMP(int(inst[1]),int(inst[2]))
+            elif inst[0] == "HLT":
+                self.HLT()
+                break
+            self.PC += 1
